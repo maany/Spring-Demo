5696a55875f6ef79c7772cfd586316ddf73e7551
Mayank Sharma <imptodefeat@gmail.com>
2015-04-13 13:38:37 +0530

    @Component + stereotypicall roles introductions @Service @Controller @Repository

--- SampleSpringModule/src/main/java/com/maany/spring/Circle.java
+++ SampleSpringModule/src/main/java/com/maany/spring/Circle.java
@@ -1,8 +1,19 @@
 package com.maany.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
 /**
  * Created by OPSKMC on 4/13/15.
  */
+@Controller
 public class Circle implements Shape {
     private Point center;
     @Override
@@ -11,6 +22,7 @@ public class Circle implements Shape {
         System.out.println("Center Point is (" + center.getX() + ", " + center.getY() + ")");
     }
 
+    @Resource(name = "pointA")
     public void setCenter(Point center) {
         this.center = center;
     }
