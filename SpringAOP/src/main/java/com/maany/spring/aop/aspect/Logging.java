655e0419aed9de0558b28b835d2235d33bf955cd
Mayank Sharma <imptodefeat@gmail.com>
2015-04-14 11:21:45 +0530

    @Pointcut pointcut expressions within logical operations

--- SpringAOP/src/main/java/com/maany/spring/aop/aspect/Logging.java
+++ SpringAOP/src/main/java/com/maany/spring/aop/aspect/Logging.java
@@ -14,14 +14,22 @@ public class Logging {
     /**
      * An aspect contains number of advices. standard AOP terminology.
      */
-    @Before("allGetters()" )
+    @Before("allCircleGetters() && allCircle()" ) //logical operations 
     public void LoggingAdvice(){
         System.out.println("Advice run. Get method called");
     }
-    @Before("allGetters()" )
+  //  @Before("allGetters()" )
     public void LoggingAdvice2(){
         System.out.println("yoyo Advice run. Get method called");
     }
     @Pointcut("execution(public * get*())")
     public void allGetters(){} // dummy method holding pointcut expression
+    @Pointcut(value = "execution(*  com.maany.spring.aop.service.Circle.*(..))")
+    public void allCircle(){}
+    @Pointcut("within(com.maany.spring.aop.service.Circle)")
+    public void allCircleGetters(){}
+    @Pointcut("within(com.maany.spring.aop.service..*)") // all packages and subpackages
+    public void allAOPGetters(){}
+
+    public void
 }
